e3e6766f957462caec3a347cd60395787e5b1756
JungHun98 <j7papa@naver.com>
2024-08-28 12:52:38 +0900

    design: 댓글 입력창 레이아웃 수정

--- src/addons/storyLineComments/container/StoryComment.tsx
+++ src/addons/storyLineComments/container/StoryComment.tsx
@@ -1,12 +1,29 @@
 import React from "react";
+import TextArea from "../components/TextArea";
+import EditButton from "../components/EditButton";
+import styled from "@emotion/styled";
+
+const Box = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 10px;
+`;
+
+const ButtonBox = styled.div``;
 
 function StoryComment() {
   // 로그인
   // open close toggle
   // post 요청 예외상황 처리
 
-  // 하위 컴포넌트 : 텍스트, 버튼
-  return <div></div>;
+  return (
+    <Box>
+      <TextArea />
+      <ButtonBox>
+        <EditButton innerText="등록" onClick={() => {}}></EditButton>
+      </ButtonBox>
+    </Box>
+  );
 }
 
 export default StoryComment;
---
+++ src/stories/StoryComment.stories.ts
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import StroyComment from "../addons/storyLineComments/container/StoryComment";
+
+const meta = {
+  title: "Example/StoryComment",
+  component: StroyComment,
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof StroyComment>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Contanier: Story = {};
